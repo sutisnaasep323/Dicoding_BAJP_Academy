8dd35ab9547d897966c2f4f9f235b690551b59a1
Asep Sutisna <sutisnaasep323@gmail.com>
2021-12-29 22:47:18 +0700

    repository

---
+++ app/src/main/java/com/example/academy/data/source/remote/response/CourseResponse.kt
@@ -0,0 +1,3 @@
+package com.example.academy.data.source.remote.response
+
+data class CourseResponse()
---
+++ app/src/main/java/com/example/academy/data/source/remote/response/ModuleResponse.kt
@@ -0,0 +1,5 @@
+package com.example.academy.data.source.remote.response
+
+data class ModuleResponse(
+    
+)
